f925e1a7d45dbb6c92ae1dcda019e204ce363ba6
Karolina Kossakowska <kossakowska.karolina1@gmail.com>
2022-02-02 12:07:18 +0100

    Add Kitchen view with MUI

--- src/components/views/Kitchen/Kitchen.js
+++ src/components/views/Kitchen/Kitchen.js
@@ -1,10 +1,48 @@
 import React from 'react';
 import styles from './Kitchen.module.scss';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+
+const demoContent = [
+  {id: '2', order: 'pizza'},
+  {id: '32', order: 'pasta'},
+  {id: '33', order: 'coffee'},
+  {id: '5', order: 'espresso and pizza'},
+  {id: '1', order: 'salad'},
+];
 
 const Kitchen = () => (
-  <div className={styles.component}>
-    <h2>Kitchen view</h2>
-  </div>
+  <Paper className={styles.component}>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Table / Order number</TableCell>
+          <TableCell>Order</TableCell>
+          <TableCell>Action</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {demoContent.map(row => (
+          <TableRow key={row.id}>
+            <TableCell component="th" scope="row">
+              {row.id}
+            </TableCell>
+            <TableCell>
+              {row.order}
+            </TableCell>
+            <TableCell>
+              <Button>Mark as done</Button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </Paper>
 );
 
 export default Kitchen;
